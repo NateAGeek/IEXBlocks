6bc88f4d365a3fc263762bd7b9f4eb2b644c3dfa
SpaceBoiNate <nathaniel@astroglu.com>
2021-07-01 18:58:45 -0500

    Quickly add mobile support.

--- src/components/IEXBlockGrid/IEXBlockGrid.tsx
+++ src/components/IEXBlockGrid/IEXBlockGrid.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, FlatList} from 'react-native';
+import React, { useMemo } from 'react';
+import {View, FlatList, useWindowDimensions} from 'react-native';
 import { IEXBlockSliceInitalState } from '../../states/redux';
 import IEXBlock from '../IEXBlock/IEXBlock';
 import IEXBlockSelector, { IEXBlockSelectorOption } from '../IEXBlockSelector/IEXBlockSelector';
@@ -23,6 +23,17 @@ export default function IEXBlockGrid({
   blocks
 }: IEXBlockGridProps): JSX.Element {
 
+  const windowWidth = useWindowDimensions().width;
+  const numCols = useMemo(() => {
+    if(windowWidth >= 1000) {
+      return 3;
+    } else if(windowWidth <= 1000 && windowWidth >= 700) {
+      return 2;
+    } else if (windowWidth <= 700){
+      return 1;
+    }
+  }, [windowWidth]);
+
   const selectionOptions: {[key: string]: IEXBlockSelectorOption} = {
     ticker: {
       title: 'Ticker Overview',
@@ -36,7 +47,7 @@ export default function IEXBlockGrid({
 
   return (
     <FlatList
-      data={blocks} numColumns={3} renderItem={(block) => (
+      data={blocks} numColumns={numCols} key={numCols} renderItem={(block) => (
         <View style={{
           flex: 1,
           margin: 16
